80eb216ee1974f00c6fcea937a1ffa3369b4d70a
Petri Montonen <91892495+PJM178@users.noreply.github.com>
2025-09-01 16:27:45 +0300

    Combine triagram similarity based search with full text search when searching for courses

--- server/src/main/java/dev/local/myproject/course/repository/CourseRepository.java
+++ server/src/main/java/dev/local/myproject/course/repository/CourseRepository.java
@@ -9,19 +9,34 @@ import jakarta.enterprise.context.ApplicationScoped;
 @ApplicationScoped
 public class CourseRepository implements PanacheRepository<Course> {
 
-    // Full text seach based on query using raw SQL - need to suppress warnings here
-    // since type cannot
-    // be inferred
+    // Full text seach and triagram similarity based on query using raw SQL - need
+    // to suppress warnings here since type cannot be inferred. Need to make sure that
+    // pg_trgm extension is installed in PostgreSQL. fts_rank and sim_score could be used
+    // to order the results in future based on what is more desired
     @SuppressWarnings("unchecked")
     public List<Course> fullTextCourseAddressSearch(String address) {
         return getEntityManager().createNativeQuery("""
-                  SELECT *, ts_rank(search_vector, plainto_tsquery('simple', :term)) AS rank
-                    FROM course
-                    WHERE search_vector @@ plainto_tsquery('simple', :term)
-                    ORDER BY rank DESC
+                SELECT *,
+                    ts_rank(search_vector, plainto_tsquery('simple', :term)) AS fts_rank,
+                    GREATEST(
+                        similarity(address, :term),
+                        similarity(city, :term),
+                        similarity(postal_code, :term)
+                    ) AS sim_score,
+                    ts_rank(search_vector, plainto_tsquery('simple', :term)) +
+                    GREATEST(
+                        similarity(address, :term),
+                        similarity(city, :term),
+                        similarity(postal_code, :term)
+                    ) as combined_score
+                FROM course
+                WHERE search_vector @@ plainto_tsquery('simple', :term)
+                    OR address % :term
+                    OR city % :term
+                    OR postal_code % :term
+                ORDER BY combined_score DESC
                 """, Course.class)
                 .setParameter("term", address)
                 .getResultList();
     }
 }
-    
